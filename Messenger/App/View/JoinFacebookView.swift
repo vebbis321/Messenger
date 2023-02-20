73414e0e31893d3e7bcafa5ee3028534bdff6c2e
Vebjørn Daniloff <veb.dan@hotmail.com>
2023-02-20 22:17:03 +0100

    Some commit

--- Messenger/App/View/JoinFacebookView.swift
+++ Messenger/App/View/JoinFacebookView.swift
@@ -25,11 +25,22 @@ struct JoinFacebookView: View {
 
                 Text("You'll need a Facebook account to use Messenger. Create an account to connect with friends, family and people who share your interests.")
                     .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.system(size: 15, weight: .regular))
 
                 AuthButtonView(title: "Get Started") {
                     print("TAppe dme")
                 }
+                
                 Spacer()
+
+                Button {
+
+                } label: {
+                    Text("Already have an account?")
+                        .font(.system(size: 13, weight: .semibold))
+                        .foregroundColor(.blue)
+                }
+
             }.padding(.horizontal, 15)
         }
     }
